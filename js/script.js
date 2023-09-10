1bd33247beebe0699196dc6460d6f185e66eb41a
Alexey Svistunov <xdayx53@gmail.com>
2023-09-10 18:16:41 +0300

    added accordion

--- js/script.js
+++ js/script.js
@@ -11,3 +11,33 @@ toggleCheckbox.addEventListener('change', function(){
     toggleElement.classList.remove('toggle--active');
   }
 });
+
+const accordions = document.querySelectorAll('.accordion');
+
+accordions.forEach(el =>  {
+  el.addEventListener('click', (e) => {
+    const self = e.currentTarget;
+
+    const accordionSwitch = self.querySelector('.accordion__switch');
+    const accordionIcon = self.querySelector('.accordion__icon');
+    const accordionContent = self.querySelector('.accordion__content');
+
+    self.classList.toggle('accordion--active');
+
+    if(self.classList.contains('accordion--active')){
+      accordionSwitch.setAttribute('aria-expanded', true);
+      accordionContent.setAttribute('aria-hidden', false);
+      // accordionIcon.classList.toggle('rotate');
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+    
+    }
+
+    else {
+      accordionSwitch.setAttribute('aria-expanded', false);
+      accordionContent.setAttribute('aria-hidden', true);
+      accordionContent.style.maxHeight = 0;
+    }
+
+  });
+
+});
