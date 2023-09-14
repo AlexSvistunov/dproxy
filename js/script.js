5393d542b4bc1af0580e5b74e902062978ef6d34
Alexey Svistunov <xdayx53@gmail.com>
2023-09-14 20:40:55 +0300

    border-radius fix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 const swiper = new Swiper('.swiper', {
   slidesPerView: '7',
   loop: true,
-  spaceBetween: 10,
-
-
+  spaceBetween: 20,
 
   navigation: {
     nextEl: '.swiper-button-next',
@@ -12,19 +10,30 @@ const swiper = new Swiper('.swiper', {
 
 });
 
+const switchToggle = document.querySelector('.switch-toggle');
+
+switchToggle.addEventListener('click', () =>{
+  switchToggle.classList.toggle('switch-toggle--eng');
+  if(switchToggle.classList.contains('switch-toggle--eng')) {
+    switchToggle.setAttribute('aria-checked', 'true');
+  } else {
+    switchToggle.setAttribute('aria-checked', 'false');
+  }
+});
+
 
 const toggleElement = document.querySelector('.toggle');
 const toggleCheckbox = document.querySelector('.toggle-input');
 
-toggleCheckbox.addEventListener('change', function(){
-  if(toggleCheckbox.checked) {
-    toggleElement.classList.add('toggle--active-eng');
-  }
+// toggleCheckbox.addEventListener('change', function(){
+//   if(toggleCheckbox.checked) {
+//     toggleElement.classList.add('toggle--active-eng');
+//   }
 
-  else {
-    toggleElement.classList.remove('toggle--active-eng');
-  }
-});
+//   else {
+//     toggleElement.classList.remove('toggle--active-eng');
+//   }
+// });
 
 const accordions = document.querySelectorAll('.accordion');
 
